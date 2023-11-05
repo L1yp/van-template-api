62777c60ef7865b6ba1e516e79cc26315629b6fe
Lyp <l1yp@qq.com>
2023-11-05 11:08:37 +0800

    feat: 新增自动生成ResultMap

--- src/main/java/org/cloud/mybatis/MybatisConfig.java
+++ src/main/java/org/cloud/mybatis/MybatisConfig.java
@@ -1,12 +1,18 @@
 package org.cloud.mybatis;
 
+import io.mybatis.provider.EntityColumn;
+import io.mybatis.provider.EntityFactory;
+import io.mybatis.provider.EntityTable;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.mapping.ResultFlag;
 import org.apache.ibatis.mapping.ResultMap;
 import org.apache.ibatis.mapping.ResultMapping;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.apache.ibatis.type.UnknownTypeHandler;
 import org.cloud.mybatis.typehandler.BasicEnumTypeHandler;
 import org.cloud.mybatis.typehandler.JacksonTypeHandler;
 import org.cloud.web.service.system.IConfigService;
@@ -21,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
 
 @Slf4j
 @org.springframework.context.annotation.Configuration
@@ -32,6 +39,9 @@ public class MybatisConfig implements ConfigurationCustomizer, ApplicationListen
 
     @Resource
     IConfigService configService;
+
+    @Resource
+    MapperAutoResultMap mapperAutoResultMap;
 
     @Override
     public void customize(Configuration configuration) {
@@ -45,6 +55,9 @@ public class MybatisConfig implements ConfigurationCustomizer, ApplicationListen
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         resetTypeHandlerInResultMapping();
+        // 自动构建resultMap
+        autoBuildResultMap(configuration, mapperAutoResultMap.getCandidateBuildResultMapTypeList());
+
     }
 
     public void resetTypeHandlerInResultMapping() {
@@ -71,6 +84,46 @@ public class MybatisConfig implements ConfigurationCustomizer, ApplicationListen
             }
 
         }
+    }
+
+    public void autoBuildResultMap(Configuration configuration, List<Class<?>> resultTypeList) {
+        for (Class<?> clazz : resultTypeList) {
+            EntityTable entityTable = EntityFactory.create(clazz);
+            ResultMap resultMap = genResultMap(configuration, entityTable);
+            configuration.addResultMap(resultMap);
+        }
+    }
+
+    public ResultMap genResultMap(Configuration configuration, EntityTable entityTable) {
+        List<ResultMapping> resultMappings = new ArrayList<>();
+        for (EntityColumn entityColumn : entityTable.selectColumns()) {
+            String column = entityColumn.column();
+            //去掉可能存在的分隔符，例如：`order`
+            Matcher matcher = EntityTable.DELIMITER.matcher(column);
+            if (matcher.find()) {
+                column = matcher.group(1);
+            }
+            ResultMapping.Builder builder = new ResultMapping.Builder(configuration, entityColumn.property(), column, entityColumn.javaType());
+            if (entityColumn.jdbcType() != null && entityColumn.jdbcType() != JdbcType.UNDEFINED) {
+                builder.jdbcType(entityColumn.jdbcType());
+            }
+            if (entityColumn.typeHandler() != null && entityColumn.typeHandler() != UnknownTypeHandler.class) {
+                try {
+                    builder.typeHandler(entityTable.getTypeHandlerInstance(entityColumn.javaType(), entityColumn.typeHandler()));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            List<ResultFlag> flags = new ArrayList<>();
+            if (entityColumn.id()) {
+                flags.add(ResultFlag.ID);
+            }
+            builder.flags(flags);
+            resultMappings.add(builder.build());
+        }
+        String resultMapId = entityTable.entityClass().getName() + "." + EntityTable.RESULT_MAP_NAME;
+        ResultMap.Builder builder = new ResultMap.Builder(configuration, resultMapId, entityTable.entityClass(), resultMappings, true);
+        return builder.build();
     }
 
 }
---
+++ src/test/java/org/cloud/MapperResultMappingTests.java
@@ -0,0 +1,18 @@
+package org.cloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@Slf4j
+@SpringBootTest
+public class MapperResultMappingTests {
+
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
